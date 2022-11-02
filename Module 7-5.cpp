bbcb879898acce4ce3d61979c9607e07821eb5ae
Andrey Gorelov <shorkun1974@yandex.ru>
2022-11-02 20:50:16 +0300

    вынес вычисления из циклов

--- Module 7-5.cpp
+++ Module 7-5.cpp
@@ -20,17 +20,21 @@ int main()
 	SetConsoleCP(1251);
 	SetConsoleOutputCP(1251);
 	
-	//Залаём ширину и высоту координатной плоскости
+	//Задаём ширину и высоту координатной плоскости
 	int widthCoordinatePlane = 20;
 	int heightCoordinatePlane = 20;
 
+	//Определяем центральные линии
+	int xCenterLine = widthCoordinatePlane / 2;
+	int yCenterLine = heightCoordinatePlane / 2;
+
 	for (int y = 1; y <= heightCoordinatePlane; y++) {
 		for (int x = 1; x <= widthCoordinatePlane; x++) {
-			if ((x == widthCoordinatePlane / 2) && (y == 1)) std::cout << "^";
-			else if (x == widthCoordinatePlane / 2) std::cout << "|";
-			else if ((y == heightCoordinatePlane / 2) && (x == widthCoordinatePlane)) std::cout << ">";
-			else if ((y == heightCoordinatePlane / 2) && (x == widthCoordinatePlane / 2)) std::cout << "+";
-			else if (y == heightCoordinatePlane / 2) std::cout << "-";
+			if (x == xCenterLine && y == 1) std::cout << "^";
+			else if (y == yCenterLine && x == widthCoordinatePlane) std::cout << ">";
+			else if (y == yCenterLine && x == xCenterLine) std::cout << "+";
+			else if (x == xCenterLine) std::cout << "|";
+			else if (y == yCenterLine) std::cout << "-";
 			else std::cout << " ";
 		}
 		std::cout << std::endl;
